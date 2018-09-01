ce909bf77aa5a7aa8017aae5aaf38d54e40dea21
sk0g <ani.mehta@mail.com>
2018-09-01 16:22:47 +1000

    Nav bar now links to pages

--- resources/views/layouts/nav-bar.blade.php
+++ resources/views/layouts/nav-bar.blade.php
@@ -1,6 +1,6 @@
 <div id="nav-bar" align="center">
-       Item1
-     | Item2
-     | Item3
+       <a href="/">Albums</a>
+     | <a href="/bands">Bands</a>
+     | <a href="/most_reviewed">Most Reviewed</a>
+     | <a href="/best_albums">Best Albums</a>
 </div>
-<br>
