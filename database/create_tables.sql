ce25a55c0939cb3a8d63c2ab3f41fe56229bec6f
sk0g <ani.mehta@mail.com>
2018-09-01 20:50:08 +1000

    Updated album art, all image URLs now >= 500x500

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -9,12 +9,12 @@ CREATE TABLE albums (
 );
 
 INSERT INTO albums VALUES(NULL, 1, "Winter's Gate", "Melodic death metal", 2016, "https://i0.wp.com/www.empireextreme.com/wp-content/uploads/2016/06/insomniumwintersgatecdnew.jpg");
-INSERT INTO albums VALUES(NULL, 1, "Shadows of the Dying Sun", "Melodic death metal", 2014, "https://upload.wikimedia.org/wikipedia/en/d/de/Shadows_of_the_Dying_Sun.jpg");
-INSERT INTO albums VALUES(NULL, 1, "One for Sorrow", "Melodic death metal", 2011, "http://www.insomnium.net/uploads/images/releases/Insomnium_One_for_Sorrow_album_cover.jpg");
+INSERT INTO albums VALUES(NULL, 1, "Shadows of the Dying Sun", "Melodic death metal", 2014, "https://www.nocleansinging.com/wp-content/uploads/2014/04/Insomnium-Shadows-of-the-Dying-Sun1.jpg");
+INSERT INTO albums VALUES(NULL, 1, "One for Sorrow", "Melodic death metal", 2011, "https://img.discogs.com/r-s_bFtyvYKu4MgEK0tNQ6m8oV4=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-3244072-1352831842-3373.jpeg.jpg");
 
 INSERT INTO albums VALUES(NULL, 2, "Evocation II (Pantheon)", "Folk", 2017, "https://imagescdn.juno.co.uk/full/CS658338-01A-BIG.jpg");
 INSERT INTO albums VALUES(NULL, 2, "Origins", "Folk metal", 2014, "http://www.ghostcultmag.com/wp-content/uploads/2014/08/eluveitie.jpg");
-INSERT INTO albums VALUES(NULL, 2, "Helvetios", "Folk metal", 2012, "https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Eluveitie_Helvetios.jpg/1024px-Eluveitie_Helvetios.jpg");
+INSERT INTO albums VALUES(NULL, 2, "Helvetios", "Folk metal", 2012, "http://www.drakkarbrasil.com.br/store/1395-thickbox_default/eluveitie-helvetios-cd-dvd.jpg");
 INSERT INTO albums VALUES(NULL, 2, "Slania", "Folk metal", 2007, "https://pre00.deviantart.net/5081/th/pre/i/2014/237/7/7/eluveitie___slania__rematered_cover__by_stygiansaviour-d7wneae.png");
 
 INSERT INTO albums VALUES(NULL, 3, "Songs From the North", "Doom metal", 2017, "http://swallowthesun.net/images/148570ce.album-sftn.jpg");
